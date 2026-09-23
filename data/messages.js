992f9fba43f0c7163b50843c8447f769fff67dfe
AdiBhawaniVM <adibhawanivm@gmail.com>
2026-09-23 16:59:02 +0530

    Check2

---
+++ data/messages.js
@@ -0,0 +1,73 @@
+export const messages = [
+  {
+    name: "Prof. Vasanta Govind Kumar Villuri",
+    designation: "Convenor, Concetto 2026",
+    image: "/about/convener.png",
+    message: [
+      "It gives immense pleasure to welcome you to CONCETTO 2026,  the techno-management festival of IIT (ISM) Dhanbad, as we celebrate the institution’s remarkable centenary journey.",
+
+      "This year’s theme, “Centauri Synapse — Forged Over a Century, Soaring Towards Infinity, reflects our enduring legacy and our aspiration to explore limitless possibilities. CONCETTO embodies the spirit of connecting knowledge with innovation, ideas with action and challenges with solutions.",
+
+      "I commend the dedicated student team for creating a platform that inspires creativity, collaboration and technological excellence. I encourage all participants to challenge conventions, exchange ideas and embrace the spirit of innovation. As IIT (ISM) Dhanbad completes a remarkable hundred years, let us honour our past, shape our present and boldly soar towards infinity."
+    ],
+  },
+
+  {
+    name: "Prof. Niptika Jana",
+    designation: "Co-Convenor, Concetto 2026",
+    image: "/about/co-convener.png",
+    message: [
+      '“The future belongs to those who believe in the beauty of their dreams.” — Eleanor Roosevelt',
+
+      "For a century, IIT (ISM) Dhanbad has stood as a place where curiosity meets knowledge, and knowledge finds its way into innovation. From its beginnings as the Indian School of Mines to its emergence as a premier institution of science, technology and management, its journey has been shaped by generations of minds that dared to explore, question and create. CONCETTO’26 Centauri Synapse carries forward this spirit. It is a celebration of ideas that challenge the familiar, creativity that transcends boundaries, and young minds that dare to imagine what comes next. As IIT (ISM) Dhanbad marks 100 years of excellence, the fest becomes a reflection of a legacy that has always looked ahead. As Co-Convenor, it is a privilege to be a part of this celebration. I extend my warmest wishes to all participants and appreciate the tireless efforts of the organising team, volunteers, supporters and sponsors who have brought this vision to life. May CONCETTO’26 be a celebration not only of what we know, but of the questions that awaken the mind and the ideas that shape tomorrow..",
+    ],
+  },
+
+  {
+    name: "Prof. Arun Udai",
+    designation: "Co-Convenor, Concetto 2026",
+    image: "/about/prof-arun-udai.png",
+    message: [
+      '“The future belongs to those who believe in the beauty of their dreams.” — — Eleanor Roosevelt',
+
+    ],
+  },
+  {
+    name: "Prof. Gopi Krishna Dondapatti",
+    designation: "Treasurer, Concetto 2026",
+    message: [
+      '“It gives me immense pleasure to be a part of Concetto’26 and to contribute towards the successful organization of this celebration of technology, innovation, and creativity. As we commemorate the Centenary Year of IIT (ISM) Dhanbad, this edition holds a special significance as we celebrate a century of excellence, legacy, and learning. As the Treasurer, I believe that responsible financial management, transparency, and accountability are essential for transforming ideas into meaningful experiences. This fest is the result of collective effort, dedication, and teamwork from every individual involved. I sincerely appreciate the commitment of the whole team and look forward to witnessing Concetto’26 become a memorable platform where ideas are shared, talents are showcased, and innovation inspires us all.”',
+
+    ],
+    image: "/about/prof-gopi-krishna.png"
+  },
+
+  {
+    name: "Sourav Dutta",
+    designation: "Student Advisor, Concetto 2026",
+    message: [
+      '“It is my immense pleasure to welcome you to Concetto 2026, the annual techno-management fest of IIT (ISM), Dhanbad—where ideas take shape, innovation takes flight, and technology meets purpose.I encourage every participant to explore, experiment, compete, and learn beyond the boundaries of the classroom. May this vibrant convergence inspire curiosity, nurture innovation, and empower young minds to address real-world challenges.Let us celebrate the spirit of discovery and the limitless potential of young minds.Best wishes to all participants and organizers for a remarkable and inspiring Concetto 2026.”'
+
+    ],
+    image: "/about/sourav-dutta.png"
+  },
+  {
+    name: "Badal Singh Naik",
+    designation: "Student Advisor, Concetto 2026",
+    message: [
+      '“Concetto stands as a vibrant platform where innovation meets imagination and ideas transform into possibilities. My vision has always been to work with dedication, embrace challenges, and strive to deliver the very best. I believe that excellence is not merely a destination, but a continuous journey driven by passion, perseverance, and teamwork. May Concetto continue to inspire young minds, encourage bold thinking, and create an environment where curiosity thrives. I extend my best wishes to the entire team for a successful and enriching edition of Concetto, filled with learning, innovation, and memorable experiences.”',
+
+    ],
+    image: "/about/badal-singh.png"
+  },
+  {
+    name: "Rahul Kumar",
+    designation: "Student Advisor, Concetto 2026",
+    message: [
+      '“It is a privilege to welcome you to Concetto 2026, the premier techno-management festival of IIT (ISM) Dhanbad. As our institute celebrates a monumental century of academic brilliance and innovation, this edition stands as a tribute to our rich legacy and a stepping stone toward a limitless future.A century of excellence has proven that progress belongs to those who dare to rethink the status quo. CONCETTO2026 stands as a testament to our institute’s enduring commitment to scientific brilliance and technical ingenuity. As part of the Advisory Committee, I am honoured to support a festival that turns ambitious concepts into tangible reality. I extend my warmest welcome to all participants. May your time here spark breakthroughs, forge lifelong connections, and inspire you to build a smarter tomorrow.”',
+
+    ],
+    image: "/about/rahul-kumar.png"
+  },
+ 
+];
