97ef9d2afb12dc67eb61f63cabf6894d549bc962
rajmahadev422 <rajmahadev422@gmail.com>
2026-09-24 16:03:11 +0530

    add messages

--- data/messages.js
+++ data/messages.js
@@ -19,7 +19,8 @@ export const messages = [
     message: [
       '“The future belongs to those who believe in the beauty of their dreams.” — Eleanor Roosevelt',
 
-      "For a century, IIT (ISM) Dhanbad has stood as a place where curiosity meets knowledge, and knowledge finds its way into innovation. From its beginnings as the Indian School of Mines to its emergence as a premier institution of science, technology and management, its journey has been shaped by generations of minds that dared to explore, question and create. CONCETTO’26 Centauri Synapse carries forward this spirit. It is a celebration of ideas that challenge the familiar, creativity that transcends boundaries, and young minds that dare to imagine what comes next. As IIT (ISM) Dhanbad marks 100 years of excellence, the fest becomes a reflection of a legacy that has always looked ahead. As Co-Convenor, it is a privilege to be a part of this celebration. I extend my warmest wishes to all participants and appreciate the tireless efforts of the organising team, volunteers, supporters and sponsors who have brought this vision to life. May CONCETTO’26 be a celebration not only of what we know, but of the questions that awaken the mind and the ideas that shape tomorrow..",
+      "For a century, IIT (ISM) Dhanbad has stood as a place where curiosity meets knowledge, and knowledge finds its way into innovation. From its beginnings as the Indian School of Mines to its emergence as a premier institution of science, technology and management, its journey has been shaped by generations of minds that dared to explore, question and create. CONCETTO’26 Centauri Synapse carries forward this spirit. It is a celebration of ideas that challenge the familiar, creativity that transcends boundaries, and young minds that dare to imagine what comes next. As IIT (ISM) Dhanbad marks 100 years of excellence, the fest becomes a reflection of a legacy that has always looked ahead. As Co-Convenor, it is a privilege to be a part of this celebration. I extend my warmest wishes to all participants and appreciate the tireless efforts of the organising team, volunteers, supporters and sponsors who have brought this vision to life.",
+      "May CONCETTO’26 be a celebration not only of what we know, but of the questions that awaken the mind and the ideas that shape tomorrow..",
     ],
   },
 
@@ -28,8 +29,11 @@ export const messages = [
     designation: "Co-Convenor, Concetto 2026",
     image: "/about/prof-arun-udai.png",
     message: [
-      '“The future belongs to those who believe in the beauty of their dreams.” — — Eleanor Roosevelt',
+      "India is now driven by a strong thrust towards startups, indigenous manufacturing, digital public infrastructure, and self-reliance in critical technologies, including medical and defence technologies. This transformation is creating unprecedented opportunities for young innovators to turn ideas into technologies, products, and enterprises that address real-world challenges.",
+      "CONCETTO 2026 provides a platform where curiosity meets technology, creativity meets entrepreneurship, and ideas evolve into meaningful solutions. As IIT (ISM) Dhanbad celebrates a century of excellence, I invite students, researchers, innovators, and industry enthusiasts to use this platform to experiment, collaborate, compete, and create.",
+      "Let us nurture a spirit of innovation that is not limited to solving problems, but aspires to build technologies and enterprises for a self-reliant, technologically empowered India.",
 
+      "Welcome to CONCETTO 2026!"
     ],
   },
   {
@@ -64,10 +68,11 @@ export const messages = [
     name: "Rahul Kumar",
     designation: "Student Advisor, Concetto 2026",
     message: [
-      '“It is a privilege to welcome you to Concetto 2026, the premier techno-management festival of IIT (ISM) Dhanbad. As our institute celebrates a monumental century of academic brilliance and innovation, this edition stands as a tribute to our rich legacy and a stepping stone toward a limitless future.A century of excellence has proven that progress belongs to those who dare to rethink the status quo. CONCETTO2026 stands as a testament to our institute’s enduring commitment to scientific brilliance and technical ingenuity. As part of the Advisory Committee, I am honoured to support a festival that turns ambitious concepts into tangible reality. I extend my warmest welcome to all participants. May your time here spark breakthroughs, forge lifelong connections, and inspire you to build a smarter tomorrow.”',
+      '“It is a privilege to welcome you to Concetto 2026, the premier techno-management festival of IIT (ISM) Dhanbad. As our institute celebrates a monumental century of academic brilliance and innovation, this edition stands as a tribute to our rich legacy and a stepping stone toward a limitless future.A century of excellence has proven that progress belongs to those who dare to rethink the status quo.',
+      'CONCETTO2026 stands as a testament to our institute’s enduring commitment to scientific brilliance and technical ingenuity. As part of the Advisory Committee, I am honoured to support a festival that turns ambitious concepts into tangible reality. I extend my warmest welcome to all participants. May your time here spark breakthroughs, forge lifelong connections, and inspire you to build a smarter tomorrow.”',
 
     ],
     image: "/about/rahul-kumar.png"
   },
- 
+
 ];
